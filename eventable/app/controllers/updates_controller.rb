86df6a46f6aa5780f774aae3492274fab19f8283
Roy Li <lizixuanroy@hotmail.com>
2015-10-06 20:50:48 -0400

    added updates

---
+++ eventable/app/controllers/updates_controller.rb
@@ -0,0 +1,51 @@
+class UpdatesController < ApplicationController
+  def index
+    @event_id = params[:event_id]
+    event = Event.find_by(id: @event_id)
+    if event.nil?
+      flash[:notice] = "Event does not exist"
+    else
+      @updates = event.updates.order('created_at DESC')
+      @event_name = event.name      
+    end            
+  end
+  
+  def new
+    @event_id= params[:event_id]
+    @event = Event.find_by(id: @event_id)
+    if @event.nil?
+      flash[:notice] = "Event does not exist"
+    end
+  end
+  
+  def destroy
+    id = params[:id]
+    event_id = params[:event_id]
+    update = Update.find_by(id: id)
+    if update.nil?
+      flash[:notice] = "Update does not exist"
+    else
+      update.destroy
+    end
+    return redirect_to action: "index", event_id: event_id    
+  end  
+
+  def create
+    description = params["description"]
+    event_id = params["event_id"]
+    event = Event.find_by(id: event_id)
+    if event.nil?
+      flash[:notice] = "Event does not exist"
+      return redirect_to action: "new"      
+    end
+    if description.nil? || description.length == 0
+      flash[:notice] = "Description is required"
+      return redirect_to action: "new", event_id: event_id      
+    end
+    update = Update.new(description: description)
+    event.updates << update
+    event.save
+    redirect_to action: "index", event_id: event_id     
+  end
+  
+end
--- eventable/config/routes.rb
+++ eventable/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :events
   resources :schedules
+  resources :updates
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
