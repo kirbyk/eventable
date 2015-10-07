701464b2dc7543f3f064e3e63239917bb0ca7bc1
Roy Li <lizixuanroy@hotmail.com>
2015-10-06 21:25:10 -0400

    added editing capability

--- eventable/app/controllers/schedules_controller.rb
+++ eventable/app/controllers/schedules_controller.rb
@@ -11,11 +11,17 @@ class SchedulesController < ApplicationController
   end
   
   def new
+    @edit = params[:edit]    
     @event_id= params[:event_id]
     @event = Event.find_by(id: @event_id)
     if @event.nil?
       flash[:notice] = "Event does not exist"
     end
+    if @edit.present?
+      @schedule_id = params[:schedule_id]
+      @schedule = Schedule.find_by(id: @schedule_id)      
+    end
+    
   end
   
   def destroy
@@ -58,11 +64,22 @@ class SchedulesController < ApplicationController
         flash[:notice] = "Please select start date and end date with valid time format"
         return redirect_to action: "new", event_id: event_id
       end            
-      # redirect to index      
-      schedule = Schedule.new(description: @description, start_time: @start_date, end_time: @end_date)
-      @event.schedules << schedule
-      @event.save
-      schedule.save
+      # redirect to index           
+      schedule_id = params[:schedule_id]
+      if schedule_id.present?
+        schedule = Schedule.find_by(id: schedule_id)
+        unless schedule.nil?
+          schedule.description = @description
+          schedule.start_time = @start_date
+          schedule.end_time = @end_date
+          schedule.save
+        end
+      else
+        schedule = Schedule.new(description: @description, start_time: @start_date, end_time: @end_date)
+        @event.schedules << schedule
+        @event.save
+        schedule.save        
+      end       
       redirect_to action: "index", event_id: event_id 
     end
   end
