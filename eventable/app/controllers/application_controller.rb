5e2695328bf24be8640a1c7cd4a82ec5df483c93
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2015-10-08 17:40:23 -0400

    Redirect to event after login

--- eventable/app/controllers/application_controller.rb
+++ eventable/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    if current_organizer.event
+      event_path(current_organizer.event)
+    else
+      new_event_path
+    end
+  end
 end
