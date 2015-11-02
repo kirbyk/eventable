2b0563422668e43baabd84bb2be0fe17326c666f
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2015-11-01 21:26:43 -0500

    Comment nonfunctional schedule factory

--- public/hackathon-template/www/js/services.js
+++ public/hackathon-template/www/js/services.js
@@ -2,22 +2,22 @@ angular.module('starter.services', [])
 
 .factory('ScheduleItems', function($http) {
 
-  var headers = {
-    'Access-Control-Allow-Origin' : '*',
-    'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
-    'Content-Type': 'application/json',
-    'Accept': 'application/json'
-  };
-
-  $http({
-    method: "GET",
-    headers: headers,
-    url: 'http://localhost:3000/events/2/schedule.json'
-  }).then(function(res) {
-    console.log(res);
-  }, function(res) {
-    console.log('Error in schedule item factory');
-  });
+  // var headers = {
+  //   'Access-Control-Allow-Origin' : '*',
+  //   'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
+  //   'Content-Type': 'application/json',
+  //   'Accept': 'application/json'
+  // };
+  //
+  // $http({
+  //   method: "GET",
+  //   headers: headers,
+  //   url: 'http://localhost:3000/events/2/schedule.json'
+  // }).then(function(res) {
+  //   console.log(res);
+  // }, function(res) {
+  //   console.log('Error in schedule item factory');
+  // });
 
   var scheduleItems = [
     {
