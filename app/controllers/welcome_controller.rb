2660d537240403e761a9bf53028c3c82c53a5643
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2015-11-05 20:31:15 -0500

    Fix welcome bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    @events = Event.where(organizer_id: current_organizer.id)
+    @events = Event.where(organizer_id: current_organizer.id) if current_organizer
   end
 end
