3a792478448729e5ede3d136c1bed2e33b467899
juankou0309 <juankou1004@gmail.com>
2015-12-10 13:26:15 -0500

    added records not found page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
   def after_sign_in_path_for(resource)
     if current_organizer.event
@@ -9,5 +10,10 @@ class ApplicationController < ActionController::Base
     else
       new_event_path
     end
+  end
+
+  private
+  def record_not_found(e)
+    render 'errors/not_found', :status => 404
   end
 end
