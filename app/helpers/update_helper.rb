b73a5be50e3ff201c3862bd088781fa89786ec98
Kirby Kohlmorgen <kirby@kkohlmorgen.com>
2015-12-10 21:52:37 -0500

    Better push notifications

--- app/helpers/update_helper.rb
+++ app/helpers/update_helper.rb
@@ -1,8 +1,27 @@
 require 'rest-client'
 
 PUSH_API = 'https://push.ionic.io/api/v1/push'
+USER_API = 'https://apps.ionic.io/api/v1/app/2e286d7e/users/query'
 
 module UpdateHelper
+  def get_user_id
+    resp = RestClient::Request.execute(method: :post, url: USER_API, 
+                                  headers: {
+                                   'Content-Type' => 'application/json',
+                                   'X-CSRFToken' => ENV['IONIC_USER_TOKEN']
+                                  },
+                                  payload: {
+                                    query: {}
+                                  }.to_json
+                                 )
+    data = JSON.parse(resp.body)
+    data['result']
+      .sort {|a,b| a['created_at'] <=> b['created_at']}
+      .reverse
+      .map {|user| user['user_id']}
+      .first
+  end
+
   def send_push_notification(ionic_id, update)
     RestClient::Request.execute(method: :post, url: PUSH_API, 
                                   headers: {
@@ -11,7 +30,7 @@ module UpdateHelper
                                    'Authorization' => ENV['IONIC_AUTH_TOKEN']
                                   },
                                   payload: {
-                                    "user_ids" => ["9ff25f8d-07a1-48d5-a718-d1e41ac1e8cd"],
+                                    "user_ids" => [get_user_id],
                                     "notification" => {
                                       "alert" => update.description
                                     }
